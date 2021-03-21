483008d3d3a54b9beabfc7d43ff2a70cfc4e68e4
Stacy Betts <stacyo23@yahoo.com>
2021-03-21 12:21:04 -0500

    fixing error text to be white

--- reactportfolio/src/components/NotFound.js
+++ reactportfolio/src/components/NotFound.js
@@ -1,14 +1,15 @@
 import { Link , useHistory} from 'react-router-dom';
-import react from "react";
+
 
 const NotFound = () => {
 const history =useHistory();
 
     return (
         <div>
-            <h2>Sorry</h2>
-            <p> That page cannot be found</p>
-            <Link to="/">Back to homepage. </Link>
+            <h2 className="text-white">Sorry</h2>
+            <p className=" text-white"> That page cannot be found</p>
+
+            <Link to="/" className=" text-white">Back to homepage. </Link>
         </div>
       );
 }
